520f02825be9833d47a88181646ce5e99f8132b8
AllenB23 <46408750+Byen23@users.noreply.github.com>
2019-01-10 06:04:40 -0800

    4.Width-Height-Area
    ask users for width, and height to calculate the area.

---
+++ 4.Width-Height-Area.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 10 06:00:01 2019
+
+@author: Byen23
+"""
+
+# This is the fourth program to be uploaded to github.
+'''This program calculated width and height to get the area.'''
+
+width = int(input("Enter with width: "))
+height = int(input("Enter with height: "))
+
+area = width * height
+print("The Area is", area, "square units.")
